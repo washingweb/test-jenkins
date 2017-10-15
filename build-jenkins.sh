d878bfcf5a3a2623e581a046a686bd52aeaca666
washwash <washingweb@hotmail.com>
2017-10-15 17:45:02 +0800

    test build-jenkins script

---
+++ build-jenkins.sh
@@ -0,0 +1,21 @@
+# 
+# branch names:
+#   prod/portal
+#   test/portal
+#   feature/portal/feature-desc
+#   jenkins/portal
+
+IFS=/ read -a branch_parts <<< $GIT_BRANCH
+
+build_type=${branch_parts[0]}
+app_name=${branch_parts[1]}
+
+assets_sub_directory=/$build_type/$app_name
+
+export APP_NAME=$app_name
+export ASSETS_PUBLIC_PATH=$assets_sub_directory/
+export OSS_PREFIX=oss://dy-app${assets_sub_directory}
+
+echo $APP_NAME
+echo $ASSETS_PUBLIC_PATH
+echo $OSS_PREFIX
